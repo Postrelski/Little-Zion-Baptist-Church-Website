05257ecbfcc046a5ef8720d08bfa8cc0c924bd4d
Matthew Postrel <matthewpostrel@gmail.com>
2023-03-28 00:42:13 -0400

    First draft

--- app.js
+++ app.js
@@ -15,3 +15,41 @@ btn.onclick = () => {
     btnText.innerHTML = "Thanks";
     btn.classList.add("active");
 };
+
+
+// -------- google map api ------ // 
+
+
+
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: 37.596680, lng: -85.910990 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 16,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerView({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+  
+}
+
+initMap();
+index.js
+
+
+// api key -- AIzaSyDpOOT7Mp2Yrl12s7fA7KNF17onwTenx1o
