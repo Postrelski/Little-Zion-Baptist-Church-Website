da9bbada6616165484b160cd931e216084a5b3ea
Matthew Postrel <matthewpostrel@gmail.com>
2023-03-27 23:48:32 -0400

    contact us page

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ const links = document.getElementById('links');
 hamburger.addEventListener('click', () => {
     links.classList.toggle('show');
 })
+
+
+
+
+const btn = document.querySelector("#btn");
+const btnText = document.querySelector("#btnText");
+
+btn.onclick = () => {
+    btnText.innerHTML = "Thanks";
+    btn.classList.add("active");
+};
